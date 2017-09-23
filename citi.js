50b2ccd6aab48815822f2a3cfbbb8181d4011dff
Daniel Pages <dspages@gmail.com>
2017-09-23 15:23:36 -0700

    set up a basic ui

---
+++ citi.js
@@ -0,0 +1,13 @@
+
+$.ajax({
+  type: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather' +
+  	'?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  success(data) {
+    console.log("We have your weather!");
+    console.log(data);
+  },
+  error() {
+    console.error("An error occurred.");
+  },
+});
